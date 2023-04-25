74cec5b53269fab47d63ecb4c6c428768cb41ede
DavidTesar <73195563+DavidTesar@users.noreply.github.com>
2023-04-25 00:40:08 -0500

    Formating and Comments

--- limon.sh
+++ limon.sh
@@ -27,6 +27,7 @@ show_art(){
     "
 }
 
+# Help banner
 show_help(){
     show_art
     echo "Usage: $0 [-h] [-p] [-i] [-r] [-c] [-o]"
@@ -65,7 +66,7 @@ rotate_info(){
         outgoing_telnet=$(cat temp | cut -d ':' -f3 | grep "^^23" | wc -l)
 
         # Display information about successful network connections
-        echo "ACTIVE NETWORK CONNECTIONS:"
+        echo "[*] ACTIVE NETWORK CONNECTIONS:"
         echo "---------------------------"
         if [ $outgoing_telnet -gt 0 ]; then
             echo $outgoing_telnet successful outgoing telnet connection.
@@ -102,34 +103,34 @@ rotate_info(){
         clear
 
         # Display the processes
-        echo "CURRENT PROCESSES:"
+        echo "[*] CURRENT PROCESSES:"
         echo "-----------------------"
         ps
         sleep 5
         clear
 
         # Display the environment variables
-        echo "ENVIRONMENT VARIABLES:"
+        echo "[*] ENVIRONMENT VARIABLES:"
         echo "-----------------------"
         printenv | tail -19
         sleep 5
         clear
 
         # Display the DNS
-        echo "DNS RESOLVING:"
+        echo "[*] DNS RESOLVING:"
         echo "-----------------------"
         cat /etc/resolv.conf
         sleep 5
         clear
 
         # Display information about current login sessions
-        echo "CURRENT LOGIN SESSIONS:"
+        echo "[*] CURRENT LOGIN SESSIONS:"
         echo "-----------------------"
         w
         echo
 
         # Display information about recent login sessions
-        echo "RECENT LOGIN SESSIONS:"
+        echo "[*] RECENT LOGIN SESSIONS:"
         echo "----------------------"
         last | head -n5
         sleep 5
@@ -141,14 +142,13 @@ rotate_info(){
         
         # touch /tmp/aliases
         # alias >> /tmp/aliases
-        # cat /tmp/aliases
-
-        echo
-        sleep 5
-        clear
+        # # cat /tmp/aliases
+        # echo
+        # sleep 5
+        # clear
 
         # Display information about aliases defined in the shell
-        echo "USERS ABLE TO LOGIN:"
+        echo "[*] USERS ABLE TO LOGIN:"
         echo "--------------------"
         grep -v -e "/bin/false" -e "/sbin/nologin" /etc/passwd | cut -d ':' -f1
         sleep 5
@@ -156,6 +156,7 @@ rotate_info(){
     done
 }
 
+# Stores a copy of files for comparison
 setup_integrity(){
     echo "[*] Copying configuration files to /limon/backup..."
     sudo mkdir -p /limon/backup/etc/
@@ -182,9 +183,10 @@ setup_integrity(){
 
     sudo mkdir -p /limon/backup/etc/bash
     sudo cp /etc/bash.bashrc /limon/backup/etc/bash/
-    echo "Done!"
+    echo "[*] Done!"
 }
 
+# Copies backed up files to their original location
 restore_integrity() {
     echo "[*] Restoring configuration files from /limon/backup..."
     sudo cp /limon/backup/etc/fstab /etc/
@@ -203,131 +205,135 @@ restore_integrity() {
     sudo cp /limon/backup/etc/network/interfaces /etc/network/
     sudo cp /limon/backup/etc/bash.bashrc /etc/bash.bashrc
     sudo cp /limon/backup/etc/profile /etc/
-    echo "Done!"
+    echo "[*] Done!"
 }
 
+# Checks and displays changes within those files
 check_integrity(){
 echo "[*] Checking integrity of configuration files..."
 # /etc/passwd
     if ! diff -q "/limon/backup/etc/passwd" "/etc/passwd" >/dev/null; then
-        echo "Changes detected in /etc/passwd"
+        echo "[*] Changes detected in /etc/passwd"
         echo "-------------------------------"
         diff --color=auto /limon/backup/etc/passwd /etc/passwd
     fi
 
 # /etc/fstab
     if ! diff -q "/limon/backup/etc/fstab" "/etc/fstab" >/dev/null; then
-        echo "Changes detected in etc/fstab"
+        echo "[*] Changes detected in etc/fstab"
         echo "-------------------------------"
         diff --color=auto /limon/backup/etc/fstab /etc/fstab
     fi
     
 # /etc/hostname
     if ! diff -q "/limon/backup/etc/hostname" "/etc/hostname" >/dev/null; then
-        echo "Changes detected in /etc/hostname"
+        echo "[*] Changes detected in /etc/hostname"
         echo "-------------------------------"
         diff --color=auto /limon/backup/etc/hostname /etc/hostname
     fi
 
 # /etc/group
     if ! diff -q "/limon/backup/etc/group" "/etc/group" >/dev/null; then
-        echo "Changes detected in /etc/group"
+        echo "[*] Changes detected in /etc/group"
         echo "-------------------------------"
         diff --color=auto /limon/backup/etc/group /etc/group
     fi
 
 # /etc/hosts
     if ! diff -q "/limon/backup/etc/hosts" "/etc/hosts" >/dev/null; then
-        echo "Changes detected in /etc/hosts"
+        echo "[*] Changes detected in /etc/hosts"
         echo "-------------------------------"
         diff --color=auto /limon/backup/etc/hosts /etc/hosts
     fi
 
 # /etc/resolv.conf
     if ! diff -q "/limon/backup/etc/resolv.conf" "/etc/resolv.conf" >/dev/null; then
-        echo "Changes detected in /etc/resolv.conf"
+        echo "[*] Changes detected in /etc/resolv.conf"
         echo "-------------------------------"
         diff --color=auto /limon/backup/etc/resolv.conf /etc/resolv.conf
     fi
 
 # /etc/sudoers
     if ! diff -q "/limon/backup/etc/sudoers" "/etc/sudoers" >/dev/null; then
-        echo "Changes detected in /etc/sudoers"
+        echo "[*] Changes detected in /etc/sudoers"
         echo "-------------------------------"
         diff --color=auto /limon/backup/etc/sudoers /etc/sudoers
     fi
 
 # /etc/ssh/sshd_config
     if ! diff -q "/limon/backup/etc/ssh/sshd_config" "/etc/ssh/sshd_config" >/dev/null; then
-        echo "Changes detected in /etc/ssh/sshd_config"
+        echo "[*] Changes detected in /etc/ssh/sshd_config"
         echo "-------------------------------"
         diff --color=auto /limon/backup/etc/ssh/sshd_config /etc/ssh/sshd_config
     fi
 
 # etc/sysctl.conf
     if ! diff -q "/limon/backup/etc/sysctl.conf" "/etc/sysctl.conf" >/dev/null; then
-        echo "Changes detected in /etc/sysctl.conf"
+        echo "[*] Changes detected in /etc/sysctl.conf"
         echo "-------------------------------"
         diff --color=auto /limon/backup/etc/sysctl.conf /etc/sysctl.conf
     fi
 
 # etc/logrotate.conf
     if ! diff -q "/limon/backup/etc/logrotate.conf" "/etc/logrotate.conf" >/dev/null; then
-        echo "Changes detected in /etc/logrotate.conf"
+        echo "[*] Changes detected in /etc/logrotate.conf"
         echo "-------------------------------"
         diff --color=auto /limon/backup/etc/logrotate.conf /etc/logrotate.conf
     fi
 
 # etc/crontab
     if ! diff -q "/limon/backup/etc/crontab" "/etc/crontab" >/dev/null; then
-        echo "Changes detected in /etc/crontab"
+        echo "[*] Changes detected in /etc/crontab"
         echo "-------------------------------"
         diff --color=auto /limon/backup/etc/crontab /etc/crontab
     fi
 
 # etc/apt/sources.list
     if ! diff -q "/limon/backup/etc/apt/sources.list" "/etc/apt/sources.list" >/dev/null; then
-        echo "Changes detected in /etc/apt/sources.list"
+        echo "[*] Changes detected in /etc/apt/sources.list"
         echo "-------------------------------"
         diff --color=auto /limon/backup/etc/apt/sources.list /etc/apt/sources.list
     fi
 
 # /etc/network/interfaces
     if ! diff -q "/limon/backup/etc/network/interfaces" "/etc/network/interfaces" >/dev/null; then
-        echo "Changes detected in /etc/network/interfaces"
+        echo "[*] Changes detected in /etc/network/interfaces"
         echo "-------------------------------"
         diff --color=auto /limon/backup/etc/network/interfaces /etc/network/interfaces
     fi
 
 # /etc/bash/bash.bashrc
     if ! diff -q "/limon/backup/etc/bash.bashrc" "/etc/bash.bashrc" >/dev/null; then
-        echo "Changes detected in /etc/bash.bashrc"
+        echo "[*] Changes detected in /etc/bash.bashrc"
         echo "-------------------------------"
         diff --color=auto /limon/backup/etc/bash.bashrc /etc/bash.bashrc
     fi
 
 # /etc/profile
     if ! diff -q "/limon/backup/etc/profile" "/etc/profile" >/dev/null; then
-        echo "Changes detected in /etc/profile"
+        echo "[*] Changes detected in /etc/profile"
         echo "-------------------------------"
         diff --color=auto /limon/backup/etc/profile /etc/profile
     fi
 }
 
-
-
-
-
+# Generates output
 generate_output(){
     echo "[*] Generating output..."
+
+    # Checks whether the output folder exists, if not it creates the path
     output_dir="/limon/outputs/"
     if [ ! -d "$output_dir" ]; then
         mkdir "$output_dir"
     fi
+
+    # Sets up log file name
     log_file="/limon/outputs/$(date +"%Y_%m_%d_%H_%M_%S").log"
     touch "$log_file"
+
+    # Pipes the output of the check function into the log file
     check_integrity | tee "$log_file"
-    echo "Report saved to $log_file"
+    echo "[*] Report saved to $log_file"
 }
 
 while getopts ":hpirco" option; do
@@ -338,6 +344,6 @@ while getopts ":hpirco" option; do
         r) restore_integrity;;
         c) check_integrity;;
         o) generate_output;;
-        *) echo "Invalid option: -${OPTARG}" >&2; show_help;;
+        *) echo "[X] Invalid option: -${OPTARG}" >&2; show_help;;
     esac
 done
